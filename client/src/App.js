3bbe199189d652c71ea358e3f8eee8bd5b3c4984
João Matos <queissotropa127@gmail.com>
2023-08-24 14:37:52 -0300

    Tangie 1.0 commit inicial

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./Scenes/homePage/index.jsx";
+import LoginPage from "./Scenes/loginPage/index.jsx";
+import ProfilePage from "./Scenes/profilePage/index.jsx";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme.js";
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Scenes/homePage/index.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import Navbar from "../navBar";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+export default HomePage;
